0dbc51f53f75b3280d8e6c46124b818ed10f9d45
Novak Boškov <gnovak.boskov@gmail.com>
2017-04-02 23:48:44 +0200

    say-hello

--- src/clojure_ntp/core.clj
+++ src/clojure_ntp/core.clj
@@ -5,3 +5,17 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn say-hello [name]
+  (println (str "Hello " name)))
+
+(say-hello "Pera")
+;; => Hello  Pera
+
+(defn square [a] (* a a))
+(defn sum-of-squares [a b]
+  (+ (square a) (square b)))
+
+(= (sum-of-squares 2 5)
+   (+ (square 2) (square 5))
+   (+ 4 25))
