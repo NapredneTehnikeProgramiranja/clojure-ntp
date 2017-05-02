9cad875f34e654c41edb1afd1aab64ce9635f5a5
Novak Boškov <gnovak.boskov@gmail.com>
2017-05-02 19:13:48 +0200

    Macros

--- src/clojure_ntp/macros.clj
+++ src/clojure_ntp/macros.clj
@@ -5,17 +5,17 @@
 
 (hebrew-lisp ((str "Hello " p) [p] foo defn))
 
-(foo "people!")n
+(foo "people!")
 ;; => Hello people!
 
 (hebrew-lisp ("people!" foo))
 ;; => Hello people!
 
-(((str "Hello " p) [p] foo defn))
+;; (((str "Hello ") [p] foo defn))
 ;; => CompilerException java.lang.RuntimeException: Unable to resolve symbol:
 ;;    p in this context, compiling:...
 
-("people!" foo)
+;; ("people!" foo)
 ;; => ClassCastException java.lang.String cannot be cast to clojure.lang.IFn
 ;;    clojure-ntp.macros/eval20385 (form-init7854233134659944327.clj:14)
 
@@ -44,3 +44,50 @@
 
 (foo "Some text")
 ;; => Some text printed from some function foo.
+
+(list? (read-string "(+ 1 2)"))
+;; => true
+
+;; -----------------------------------------------------------------------------
+;; Some macros
+
+(read-string "#(println %)")
+;; => (fn* [p1__20475#] (println p1__20475#))
+
+(read-string "'(1 2 3)")
+;; => (quote (1 2 3))
+
+(read-string "@a")
+;; => (clojure.core/deref a)
+
+(read-string "; this is the comment\n(+ 1 2)")
+;; => (+ 1 2)
+
+;; -----------------------------------------------------------------------------
+;; Macros detailed
+
+(defmacro infix [form]
+  (list
+   (second form)
+   (first form)
+   (last form)))
+
+(infix (1 + 2))
+;; => 3
+
+(read-string "(defmacro hebrew-lisp [form] (reverse form))")
+;; => (defmacro hebrew-lisp [form] (reverse form))
+
+(eval (read-string "(defmacro hebrew-lisp [form] (reverse form))"))
+;; => #'clojure-ntp.macros/hebrew-lisp
+
+(macroexpand (read-string "(hebrew-lisp ((str \"Hello \" p) [p] foo defn))"))
+;; => (def foo (clojure.core/fn ([p] (str "Hello " p))))
+
+(-> "(hebrew-lisp ((str \"Hello \" p) [p] foo defn))"
+    read-string
+    macroexpand
+    eval)
+
+(foo "good people")
+;; => Hello good people
