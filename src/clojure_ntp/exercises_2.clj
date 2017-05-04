2744fb06ae66c36e8e211e4c6a6606c24c4c83ef
Novak Boškov <gnovak.boskov@gmail.com>
2017-05-04 12:37:03 +0200

    exercises 2

---
+++ src/clojure_ntp/exercises_2.clj
@@ -0,0 +1,20 @@
+(ns clojure-ntp.exercises-2)
+
+(defmacro дефиниши-ф [name params form]
+  `(defn ~name ~(vec params) ~form))
+
+(дефиниши-ф здраво
+  []
+  (println "Здраво свима!"))
+
+(здраво)
+;; => Здраво свима!
+
+(дефиниши-ф аритметичка-средина
+  [brojevi]
+  (/ (apply + brojevi) (-> brojevi
+                           count
+                           double)))
+
+(аритметичка-средина (range 10))
+;; => 4.5
